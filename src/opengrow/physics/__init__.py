0c66eefd90d373efe690d2bce39e80e9c3a02651
Chris Sevilla <62484959+ChrisSeVa1@users.noreply.github.com>
2026-09-04 00:31:56 +0200

    feat: export manufacturer optics abstractions

--- src/opengrow/physics/__init__.py
+++ src/opengrow/physics/__init__.py
@@ -1,5 +1,17 @@
 """Photon-domain physics functions."""
 
+from .photometry import AngularDistribution, IESMetadata, load_ies, parse_ies_text
 from .photons import dli_from_ppfd, irradiance_to_photon_flux
+from .spectrum import TabulatedSpectrum, load_spectrum, parse_spectrum_text
 
-__all__ = ["dli_from_ppfd", "irradiance_to_photon_flux"]
+__all__ = [
+    "AngularDistribution",
+    "IESMetadata",
+    "TabulatedSpectrum",
+    "dli_from_ppfd",
+    "irradiance_to_photon_flux",
+    "load_ies",
+    "load_spectrum",
+    "parse_ies_text",
+    "parse_spectrum_text",
+]
